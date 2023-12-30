26f77cce9b9bd7368e18933ba43bc8177d280bdf
Kalpani1234 <Kalpani4lk@gmail.com>
2023-12-30 16:21:04 +0530

    Add function for displayTimer

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ document.getElementById("timer-reset").addEventListener("click", () => {
     [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
     timeRef.innerHTML = "00 : 00 : 00 : 000 ";
 }); 
+
+function displayTimer() {
+    milliseconds += 10;
+    if(milliseconds == 1000) {
+        milliseconds = 0;
+        seconds++;
+        if(seconds == 60) {
+            seconds = 0;
+            minutes++;
+            if(minutes == 60) {
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
